c7cc49dfa01ffb8d8d80b7a7bd8e163c8c78235a
IldarAbdulin <abdulinildar1@gmail.com>
2023-01-31 13:26:55 +0600

    lesson 5

--- src/App.js
+++ src/App.js
@@ -1,41 +1,106 @@
 import React, { useState, useEffect } from 'react';
-
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state =  {text: ''}
-  }
-  updatedText = (newText) => {
-      this.setState(() => ({
-        text: newText
-      }));
-  }
-  render() {
-      return (
-          <div>
-              <div>
-                  <input type="text" placeholder="Your message here.." onChange={(e) => this.updatedText(e.target.value)} />
-                  <p>the message is: {this.state.text}</p>
-              </div>
-          </div>
-      );
-  }
-}
+import { useForm } from 'react-hook-form';
 
 // function App() {
-//   const [text, setText] = useState('');
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
+//   const [users, setUsers] = useState([]);
+//   const [name, setName] = useState('');
+//   const [lastName, setLastName] = useState('');
+//   const handleSubmit = (e) => e.preventDefault();
+//   const addUser = () => {
+//     if(name === '' || lastName === '') {
+//       return alert('Запоните все поля')
+//     }
+//     const user = {
+//       id: Math.random() * 1000000000,
+//       name: name,
+//       lastName: lastName,
+//     };
+//     setUsers([...users, user]);
+//     setName('');
+//     setLastName('');
 //   };
-//   const handleChange = (e) => setText(e.target.value);
 //   return (
 //     <div>
-//       <form onSubmit={handleSubmit}>
-//         <input placeholder="Введите текст" onChange={handleChange} />
+//       <form className='form' onSubmit={handleSubmit}>
+//         <input
+//           placeholder="Введите ваше имя"
+//           value={name}
+//           onChange={(e) => setName(e.target.value)}
+//         />
+//         <input
+//           placeholder="Введите вашу фамилию"
+//           value={lastName}
+//           onChange={(e) => setLastName(e.target.value)}
+//         />
+//         <input
+//           type={'submit'}
+//           onClick={addUser}
+//           value={`Добавить пользователя`}
+//         />
 //       </form>
-//       <h2>{text === '' ? 'Ваш текст будет тут' : text}</h2>
+//       <div className="user-list">
+//         {users &&
+//           users.map((user) => (
+//             <div className="user" key={user.id}>
+//               <p>Имя пользователя: {user.name}</p>
+//               <p>Фамилия пользователя: {user.lastName}</p>
+//             </div>
+//           ))}
+//       </div>
 //     </div>
 //   );
 // }
+
+function App() {
+  const { register, handleSubmit } = useForm();
+  const [users, setUsers] = useState([]);
+  const [name, setName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const addUser = () => {
+    if (name === '' || lastName === '') {
+      return alert('Запоните все поля');
+    }
+    const user = {
+      id: Math.random() * 1000000000,
+      name: name,
+      lastName: lastName,
+    };
+    setUsers([...users, user]);
+    setName('');
+    setLastName('');
+  };
+  return (
+    <>
+      <form className="form" onSubmit={handleSubmit()}>
+        <input
+          {...register('firstName')}
+          placeholder="Имя"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        <input
+          {...register('lastName')}
+          placeholder="Фамилия"
+          onChange={(e) => setLastName(e.target.value)}
+          value={lastName}
+        />
+        <input
+          type="submit"
+          value={`Добавить пользователя`}
+          onClick={addUser}
+        />
+      </form>
+      <div className="user-list">
+        {users &&
+          users.map((user) => (
+            <div className="user" key={user.id}>
+              <p>Имя пользователя: {user.name}</p>
+              <p>Фамилия пользователя: {user.lastName}</p>
+            </div>
+          ))}
+      </div>
+    </>
+  );
+}
 
 export default App;
