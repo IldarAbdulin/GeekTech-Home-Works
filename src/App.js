85c5322bd562a63c71ade002d16e7740913e1464
IldarAbdulin <abdulinildar1@gmail.com>
2023-02-03 22:26:48 +0600

    home work №6

--- src/App.js
+++ src/App.js
@@ -1,92 +1,43 @@
-import React, { useState, useEffect } from 'react';
-import { useForm } from 'react-hook-form';
+import React from 'react';
 
-// function App() {
-//   const [users, setUsers] = useState([]);
-//   const [name, setName] = useState('');
-//   const [lastName, setLastName] = useState('');
-//   const handleSubmit = (e) => e.preventDefault();
-//   const addUser = () => {
-//     if(name === '' || lastName === '') {
-//       return alert('Запоните все поля')
-//     }
-//     const user = {
-//       id: Math.random() * 1000000000,
-//       name: name,
-//       lastName: lastName,
-//     };
-//     setUsers([...users, user]);
-//     setName('');
-//     setLastName('');
-//   };
-//   return (
-//     <div>
-//       <form className='form' onSubmit={handleSubmit}>
-//         <input
-//           placeholder="Введите ваше имя"
-//           value={name}
-//           onChange={(e) => setName(e.target.value)}
-//         />
-//         <input
-//           placeholder="Введите вашу фамилию"
-//           value={lastName}
-//           onChange={(e) => setLastName(e.target.value)}
-//         />
-//         <input
-//           type={'submit'}
-//           onClick={addUser}
-//           value={`Добавить пользователя`}
-//         />
-//       </form>
-//       <div className="user-list">
-//         {users &&
-//           users.map((user) => (
-//             <div className="user" key={user.id}>
-//               <p>Имя пользователя: {user.name}</p>
-//               <p>Фамилия пользователя: {user.lastName}</p>
-//             </div>
-//           ))}
-//       </div>
-//     </div>
-//   );
-// }
+import firstImg from './assets/1.jpg'
+import secondImg from './assets/2.webp'
+import thirdImg from './assets/3.jpg'
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
 
 function App() {
-  const {
-    register,
-    formState: { errors },
-    handleSubmit,
-  } = useForm();
-  const [users, setUsers] = useState('');
   return (
-    <>
-      <form
-        className="form"
-        onSubmit={handleSubmit((user) => setUsers(JSON.stringify(user)))}
-      >
+    <Swiper
+      modules={[Navigation]}
+      spaceBetween={0}
+      slidesPerView={1}
+      navigation
+    >
+      <SwiperSlide>
         <div>
-          <input
-            {...register('firstName', {
-              required: 'Поле обязательно к заполнению',
-            })}
-            placeholder="First name"
-          />
-          {errors?.firstName && <p>{errors?.firstName.message}</p>}
+          <img src={firstImg} />
+          <h1>Slide 1</h1>
         </div>
-        <div>
-          <input
-            {...register('lastName', {
-              required: 'Поле обязательно к заполнению',
-            })}
-            placeholder="Last name"
-          />
-          {errors?.lastName && <p>{errors?.lastName.message}</p>}
+      </SwiperSlide>
+      <SwiperSlide>
+      <div>
+          <img src={secondImg} />
+          <h1>Slide 2</h1>
         </div>
-        <input type="submit" />
-      </form>
-      <div style={{ textAlign: 'center', padding: 20 }}>{users}</div>
-    </>
+      </SwiperSlide>
+      <SwiperSlide>
+      <div>
+          <img src={thirdImg} />
+          <h1>Slide 3</h1>
+        </div>
+      </SwiperSlide>
+    </Swiper>
   );
-}
+};
 
 export default App;
