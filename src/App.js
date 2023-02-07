690dce25093618c9ee9c4ee6999061555c775dd5
IldarAbdulin <abdulinildar1@gmail.com>
2023-02-07 20:15:23 +0600

    hw 7

--- src/App.js
+++ src/App.js
@@ -1,49 +1,51 @@
 import axios from 'axios';
 import React from 'react';
 
-// function App() {
-//   const [date, setDate] = React.useState([]);
-//   React.useEffect(() => {
-//     axios
-//       .get('https://jsonplaceholder.typicode.com/posts')
-//       .then(({ data }) => setDate(data));
-//   }, []);
-//   return (
-//     <>
-//       {!date.length ? (
-//         <h1>Loading...</h1>
-//       ) : (
-//         <div>
-//           <h1 style={{color:'red'}}>POSTS:</h1>
-//           {date.map((item) => (
-//             <div key={item.id}>
-//               <p>{item.title}</p>
-//             </div>
-//           ))}
-//         </div>
-//       )}
-//     </>
-//   );
-// }
-
+//1 example
 function App() {
-  const [date, setDate] = React.useState(new Date())
-  const tick = () => {
-    setDate(new Date())
-  }
+  const [date, setDate] = React.useState([]);
   React.useEffect(() => {
-    const timerId = setInterval(() => {
-      tick()
-    }, 1000)
-    return () => {
-      clearInterval(timerId)
-    }
-  }, [date])
+    axios
+      .get('https://jsonplaceholder.typicode.com/posts')
+      .then(({ data }) => setDate(data));
+  }, []);
   return (
-    <div>
-      <h2>Сейчас: {date.toLocaleTimeString()}</h2>
-    </div>
-  )
+    <>
+      {!date.length ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <h1 style={{color:'red'}}>POSTS:</h1>
+          {date.map((item) => (
+            <div key={item.id}>
+              <p>{item.title}</p>
+            </div>
+          ))}
+        </div>
+      )}
+    </>
+  );
 }
+
+//2 example
+// function App() {
+//   const [date, setDate] = React.useState(new Date())
+//   const tick = () => {
+//     setDate(new Date())
+//   }
+//   React.useEffect(() => {
+//     const timerId = setInterval(() => {
+//       tick()
+//     }, 1000)
+//     return () => {
+//       clearInterval(timerId)
+//     }
+//   }, [date])
+//   return (
+//     <div>
+//       <h2>Сейчас: {date.toLocaleTimeString()}</h2>
+//     </div>
+//   )
+// }
 
 export default App;
