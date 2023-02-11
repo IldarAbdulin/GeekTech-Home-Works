c3497a5a58d8d46d5207cd5f2ecf2345551e9323
IldarAbdulin <abdulinildar1@gmail.com>
2023-02-11 09:28:17 +0600

    hw 8

--- src/App.js
+++ src/App.js
@@ -1,51 +1,24 @@
-import axios from 'axios';
 import React from 'react';
 
-//1 example
-function App() {
-  const [date, setDate] = React.useState([]);
-  React.useEffect(() => {
-    axios
-      .get('https://jsonplaceholder.typicode.com/posts')
-      .then(({ data }) => setDate(data));
-  }, []);
+const Button = ({onClick, children}) => {
   return (
-    <>
-      {!date.length ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          <h1 style={{color:'red'}}>POSTS:</h1>
-          {date.map((item) => (
-            <div key={item.id}>
-              <p>{item.title}</p>
-            </div>
-          ))}
-        </div>
-      )}
-    </>
-  );
+    <button onClick={onClick}>{children}</button>
+  )
 }
-
-//2 example
-// function App() {
-//   const [date, setDate] = React.useState(new Date())
-//   const tick = () => {
-//     setDate(new Date())
-//   }
-//   React.useEffect(() => {
-//     const timerId = setInterval(() => {
-//       tick()
-//     }, 1000)
-//     return () => {
-//       clearInterval(timerId)
-//     }
-//   }, [date])
-//   return (
-//     <div>
-//       <h2>Сейчас: {date.toLocaleTimeString()}</h2>
-//     </div>
-//   )
-// }
-
+function App() {
+  const [count , setCounter] = React.useState(0)
+  const handlePlus = () => {
+    setCounter(count + 1)
+  }
+  const handleMinus = () => {
+    setCounter(count - 1)
+  }
+  return (
+    <div>
+      <Button onClick={handlePlus}>Plus</Button>
+      <span>{count}</span>
+      <Button onClick={handleMinus}>Minus</Button>
+    </div>
+  )
+}
 export default App;
